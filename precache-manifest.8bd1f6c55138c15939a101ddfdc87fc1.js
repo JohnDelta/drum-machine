99ac8e68e7f9d0ce05ce4165e26e4fa14f647584
John Delta <john.deligiannis1@gmail.com>
2019-08-04 18:29:28 +0300

    Updates

---
+++ precache-manifest.8bd1f6c55138c15939a101ddfdc87fc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d115fdd58c58b9b2b0281e05bdc61fb7",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "f1d089f93a374e2db9b6",
+    "url": "/drum-machine/static/css/main.1b3c4562.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/drum-machine/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "f1d089f93a374e2db9b6",
+    "url": "/drum-machine/static/js/main.2271b624.chunk.js"
+  },
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  }
+]);
